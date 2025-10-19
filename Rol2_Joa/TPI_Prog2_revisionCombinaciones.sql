3a2b60302acb3746d3ae77c6dd6c7639c5950843
Joygamarra09 <yoanagamarra09@gmail.com>
2025-10-19 03:10:32 -0300

    Update SQL query limit from 5 to 15

--- Rol2_Joa/TPI_Prog2_revisionCombinaciones.sql
+++ Rol2_Joa/TPI_Prog2_revisionCombinaciones.sql
@@ -1,4 +1,4 @@
 use usuariocredencial;
 SELECT COUNT(*) FROM usuario;
-SELECT * FROM usuario ORDER BY id DESC LIMIT 15; -- Revisa los últimos 5
+SELECT * FROM usuario ORDER BY id DESC LIMIT 15; -- Revisa los últimos 15
 SELECT * FROM credencial ORDER BY id_usuario DESC LIMIT 15;
